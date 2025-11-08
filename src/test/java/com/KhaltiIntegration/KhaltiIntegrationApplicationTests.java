7528cc30f8b9e3e2b71e515824be18325f824036
AnishShrestha <anishnitro5@gmail.com>
2025-11-08 16:13:03 +0545

    Initial commit

---
+++ src/test/java/com/KhaltiIntegration/KhaltiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KhaltiIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhaltiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
